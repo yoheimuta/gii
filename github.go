63c3b7058c9d9ee50b77b32a151e1f72072aa68a
yoheimuta <yoheimuta@gmail.com>
2015-04-30 22:52:47 +0900

    Import gists to github issues

--- github.go
+++ github.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 
+	"golang.org/x/oauth2"
+
 	"github.com/google/go-github/github"
-	//"github.com/k0kubun/pp"
+	"github.com/k0kubun/pp"
 )
 
 type GitHub struct {
-	client *github.Client
+	client    *github.Client
+	isVerbose bool
 }
 
 type GistInfo struct {
@@ -16,14 +19,31 @@ type GistInfo struct {
 	comments []github.GistComment
 }
 
-func CreateGitHub() *GitHub {
+// tokenSource is an oauth2.TokenSource which returns a static access token
+type tokenSource struct {
+	token *oauth2.Token
+}
+
+// Token implements the oauth2.TokenSource interface
+func (t *tokenSource) Token() (*oauth2.Token, error) {
+	return t.token, nil
+}
+
+func CreateGitHub(token string, isVerbose bool) *GitHub {
+	ts := &tokenSource{
+		&oauth2.Token{AccessToken: token},
+	}
+
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
+
 	g := &GitHub{
-		client: github.NewClient(nil),
+		client:    github.NewClient(tc),
+		isVerbose: isVerbose,
 	}
 	return g
 }
 
-func (g *GitHub) GetIssues(gistIds []string) (gistInfo []GistInfo, err error) {
+func (g *GitHub) GetGists(gistIds []string) (gistsInfo []GistInfo, err error) {
 	for _, id := range gistIds {
 		fmt.Printf("Downloading a gist and comments: %v\n", id)
 
@@ -33,10 +53,12 @@ func (g *GitHub) GetIssues(gistIds []string) (gistInfo []GistInfo, err error) {
 			continue
 		}
 		if res.StatusCode != 200 {
-			fmt.Printf("Skipped against a invalid response of Gists.Get API: %v\n", res.StatusCode)
+			fmt.Printf("Skipped against an invalid response of Gists.Get API: %v\n", res.StatusCode)
 			continue
 		}
-		//pp.Print(gist)
+		if g.isVerbose {
+			pp.Println(gist)
+		}
 
 		comments, res1, err1 := g.client.Gists.ListComments(id, &github.ListOptions{})
 		if err1 != nil {
@@ -47,15 +69,158 @@ func (g *GitHub) GetIssues(gistIds []string) (gistInfo []GistInfo, err error) {
 			fmt.Printf("Skipped against a invalid response of Gists.ListComments API: %v\n", res1.StatusCode)
 			continue
 		}
-		//pp.Print(comments)
+		if g.isVerbose {
+			pp.Println(comments)
+		}
 
-		gistInfo = append(gistInfo, GistInfo{
+		gistsInfo = append(gistsInfo, GistInfo{
 			gist:     gist,
 			comments: comments,
 		})
 
-		fmt.Printf("Downloaded a gist and comments: %v\n", id)
+		fmt.Printf("Downloaded  a gist and comments: %v\n", id)
 	}
 
-	return gistInfo, nil
+	return gistsInfo, nil
+}
+
+func (g *GitHub) ImportGistsToIssues(gistsInfo []GistInfo, repo string, dry_run bool) (processed_count int, err error) {
+	for _, gistInfo := range gistsInfo {
+		var gist map[string]*string
+
+		gist, err = g.extractGist(gistInfo.gist)
+		if err != nil {
+			fmt.Printf("Failed to extract gist: %v. Skipped\n", err)
+			continue
+		}
+
+		var issue *github.Issue
+
+		if !dry_run {
+			owner := *gist["Owner"]
+			body := fmt.Sprintf("Automatically imported from %s.\n\n%s", *gist["URL"], *gist["Content"])
+
+			var res *github.Response
+			issue, res, err = g.client.Issues.Create(owner, repo, &github.IssueRequest{
+				Title: gist["Title"],
+				Body:  &body,
+			})
+
+			if err != nil {
+				fmt.Printf("Skipped against a failed Issues.Create API: %v\n", err)
+				continue
+			}
+			if res.StatusCode != 201 {
+				fmt.Printf("Skipped against an invalid response of Issues.Create API: %v\n", res.StatusCode)
+				continue
+			}
+
+			fmt.Printf("Created an issue: %v\n", *issue.Number)
+			if g.isVerbose {
+				//TODO: pp panicked
+				//pp.Println(*issue)
+				//pp.Println(*res)
+			}
+		} else {
+			fmt.Println("Dry-run to create an issue")
+			if g.isVerbose {
+				pp.Println(gist)
+			}
+		}
+
+		var comment map[string]*string
+		for _, gistComment := range gistInfo.comments {
+			comment, err = g.extractGistComment(gistComment)
+			if err != nil {
+				fmt.Printf("Skipped. Failed to extract gistComment: %v\n", err)
+				continue
+			}
+			if !dry_run {
+				number := *issue.Number
+				commentOwner := *comment["Owner"]
+
+				var issueComment *github.IssueComment
+				var res *github.Response
+				issueComment, res, err = g.client.Issues.CreateComment(commentOwner, repo, number, &github.IssueComment{
+					Body: comment["Body"],
+				})
+
+				if err != nil {
+					fmt.Printf("Skipped against a failed Issues.CreateComment API: %v\n", err)
+					continue
+				}
+				if res.StatusCode != 201 {
+					fmt.Printf("Skipped against an invalid response of Issues.CreateComment API: %v\n", res.StatusCode)
+					continue
+				}
+
+				fmt.Printf("Created an comment: %v\n", *issueComment.ID)
+				if g.isVerbose {
+					//TODO: pp panicked
+					//pp.Println(issueComment)
+					//pp.Println(res)
+				}
+			} else {
+				fmt.Println("Dry-run to create a comment")
+				if g.isVerbose {
+					pp.Println(comment)
+				}
+			}
+		}
+		processed_count++
+	}
+	return processed_count, nil
+}
+
+func (g *GitHub) extractGist(gist *github.Gist) (extracted map[string]*string, err error) {
+	var gistTitle *string
+	var gistContent *string
+	for title, file := range gist.Files {
+		gistTitle0 := string(title)
+		gistTitle = &gistTitle0
+		gistContent = file.Content
+	}
+	gistOwner := gist.Owner.Login
+	gistHTMLURL := gist.HTMLURL
+
+	if *gistTitle == "" {
+		return nil, fmt.Errorf("Not Found gistTitle")
+	}
+	if *gistContent == "" {
+		return nil, fmt.Errorf("Not Found gistContent")
+	}
+	if *gistOwner == "" {
+		return nil, fmt.Errorf("Not Found gistOwner")
+	}
+	if *gistHTMLURL == "" {
+		return nil, fmt.Errorf("Not Found gistHTMLURL")
+	}
+
+	extracted = map[string]*string{
+		"Title":   gistTitle,
+		"Content": gistContent,
+		"Owner":   gistOwner,
+		"URL":     gistHTMLURL,
+	}
+
+	return extracted, nil
+}
+
+func (g *GitHub) extractGistComment(comment github.GistComment) (extracted map[string]*string, err error) {
+	commentBody := comment.Body
+	commentOwner := comment.User.Login
+
+	if *commentBody == "" {
+		return nil, fmt.Errorf("Not Found commentBody")
+	}
+	if *commentOwner == "" {
+		return nil, fmt.Errorf("Not Found commentOwner")
+	}
+
+	extracted = map[string]*string{
+		"Body":  commentBody,
+		"Owner": commentOwner,
+	}
+
+	return extracted, nil
 }
--- go-from-gist-to-issue.go
+++ go-from-gist-to-issue.go
@@ -13,26 +13,69 @@ func main() {
 	app.Name = "go-from-gist-to-issue"
 	app.Usage = "importing gists to github issues"
 	app.EnableBashCompletion = true
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "gist",
+			Value: "",
+			Usage: "a text file to list up gist url",
+		},
+		cli.StringFlag{
+			Name:  "repo",
+			Value: "",
+			Usage: "a repository name to be imported from gists",
+		},
+		cli.StringFlag{
+			Name:  "token",
+			Value: "",
+			Usage: "a github personal access token",
+		},
+		cli.BoolFlag{
+			Name:  "dry-run",
+			Usage: "a flag to run without any changes",
+		},
+		cli.BoolFlag{
+			Name:  "verbose",
+			Usage: "a flag to log verbosely",
+		},
+	}
 	app.Action = func(c *cli.Context) {
-		if len(c.Args()) < 1 {
-			fmt.Println("Not Found an argument of a filename")
+		if c.String("gist") == "" {
+			fmt.Println("Not Found an argument of a gist")
 			return
 		}
+		if c.String("repo") == "" {
+			fmt.Println("Not Found an argument of a repo")
+			return
+		}
+		if c.String("token") == "" {
+			fmt.Println("Not Found an argument of a token")
+			return
+		}
+		isVerbose := c.Bool("verbose")
 
-		gistIds, err := Parse(c.Args()[0])
+		gistIds, err := Parse(c.String("gist"))
 		if err != nil {
 			fmt.Printf("Failed to parse a given file: err=%v\nAborted.\n", err)
 			return
 		}
-		pp.Print(gistIds)
 
-		github := CreateGitHub()
-		gistInfo, err1 := github.GetIssues(gistIds)
+		if isVerbose {
+			pp.Println(gistIds)
+		}
+
+		github := CreateGitHub(c.String("token"), isVerbose)
+		gistsInfo, err1 := github.GetGists(gistIds)
 		if err1 != nil {
-			fmt.Printf("Failed to get gist info: err=%v\nAborted.\n", err1)
+			fmt.Printf("Failed to get gists info: err=%v\nAborted.\n", err1)
 			return
 		}
-		fmt.Printf("GistInfo=%v\n", gistInfo)
+
+		imported_count, err2 := github.ImportGistsToIssues(gistsInfo, c.String("repo"), c.Bool("dry-run"))
+		if err2 != nil {
+			fmt.Printf("Failed to import gists info: err=%v\nAborted.\n", err2)
+			return
+		}
+		fmt.Printf("Completed to import gists info: count=%v\n", imported_count)
 	}
 	app.Run(os.Args)
 }
